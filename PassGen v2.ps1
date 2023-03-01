244c85482fc95122943c14c5dc9a39790f4ac058
Chronos Alpha <126511546+MilSpec-IT@users.noreply.github.com>
2023-03-01 14:29:07 -0500

    Add files via upload

---
+++ PassGen v2.ps1
@@ -0,0 +1,21 @@
+﻿# Read in the list of verbs and nouns
+$verbs = Get-Content "C:\Users\cka\Documents\Automation\PowerShell\PassGen\verbs.txt"
+$nouns = Get-Content "C:\Users\cka\Documents\Automation\PowerShell\PassGen\nouns.txt"
+$specials = @("!", "@", "#", "$")
+
+# Generate a random verb and noun
+$verb = (Get-Random -InputObject $verbs)
+$noun = (Get-Random -InputObject $nouns)
+
+# Generate two random numbers between 0 and 9
+$num1 = Get-Random -Minimum 0 -Maximum 10
+$num2 = Get-Random -Minimum 0 -Maximum 10
+
+# Generate a random special character
+$special = Get-Random -InputObject $specials
+
+# Concatenate the parts to form the password
+$password = "$verb$noun$num1$num2$special"
+
+# Output the password
+Write-Output $password
---
+++ PassGen.ps1
@@ -0,0 +1,20 @@
+﻿$verbs = @("jump", "run", "skip", "dance", "sing", "swim", "walk", "write", "talk", "fly")
+$nouns = @("apple", "banana", "car", "dog", "tree", "phone", "book", "flower", "chair", "shoe")
+$specials = @("!", "@", "#", "$")
+
+# Generate a random verb and noun
+$verb = Get-Random -InputObject $verbs
+$noun = Get-Random -InputObject $nouns
+
+# Generate two random numbers between 0 and 9
+$num1 = Get-Random -Minimum 0 -Maximum 10
+$num2 = Get-Random -Minimum 0 -Maximum 10
+
+# Generate a random special character
+$special = Get-Random -InputObject $specials
+
+# Concatenate the parts to form the password
+$password = "$verb$noun$num1$num2$special"
+
+# Output the password
+Write-Output $password
